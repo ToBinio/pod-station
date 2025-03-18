220ffe6d23558c916da20506cafa7f9caeebeea1
tobinio <Tobias.frischmann1@gmail.com>
2025-03-18 16:33:52 +0100

    show history graph via unovis

--- frontend/components/Container.vue
+++ frontend/components/Container.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { VisXYContainer, VisLine, VisAxis, VisBulletLegend } from "@unovis/vue";
 const { container } = defineProps<{ container: Container }>();
 
 function getUptimeText(): string {
@@ -16,6 +17,29 @@ const uptime = ref(getUptimeText());
 useIntervalFn(() => {
     uptime.value = getUptimeText();
 }, 1000);
+
+interface DataRecord {
+    time_stamp: number;
+    mem_percentage: number;
+    cpu_percentage: number;
+}
+
+const data = ref<DataRecord[]>([]);
+
+watch(
+    () => container,
+    (new_data) => {
+        data.value.push({
+            time_stamp: Date.now(),
+            mem_percentage: new_data.memory_percent,
+            cpu_percentage: new_data.cpu_percent,
+        });
+
+        if (data.value.length > 60) {
+            data.value.shift();
+        }
+    },
+);
 </script>
 
 <template>
@@ -30,6 +54,40 @@ useIntervalFn(() => {
                 memory: {{ container.memory_percent }}%
                 {{ container.memory_usage }}
             </div>
+        </div>
+        <div>
+            <div class="flex flex-row justify-center">
+                <VisBulletLegend
+                    :items="[{ name: 'Memory' }, { name: 'CPU' }]"
+                />
+            </div>
+            <VisXYContainer :data="data" class="h-48">
+                <VisLine
+                    :x="(data: DataRecord) => data.time_stamp"
+                    :y="[
+                        (data: DataRecord) => data.mem_percentage,
+                        (data: DataRecord) => data.cpu_percentage,
+                    ]"
+                    :duration="0"
+                />
+                <VisAxis
+                    type="x"
+                    :tickFormat="
+                        (value: number) =>
+                            Intl.DateTimeFormat('en-US', {
+                                hour: 'numeric',
+                                minute: 'numeric',
+                                second: 'numeric',
+                                hourCycle: 'h23',
+                            }).format(value)
+                    "
+                />
+                <VisAxis
+                    type="y"
+                    :position="'left'"
+                    :tickFormat="(value: number) => value + '%'"
+                />
+            </VisXYContainer>
         </div>
     </div>
 </template>
--- justfile
+++ justfile
@@ -2,9 +2,9 @@ mod backend "backend/justfile"
 mod frontend "frontend/justfile"
 
 run-test-container:
-    podman run --rm -d --name test-container-1 alpine sh -c "trap 'exit 0' TERM; while true; do sleep 1; done"
-    podman run --rm -d --name test-container-2 alpine sh -c "trap 'exit 0' TERM; while true; do sleep 1; done"
-    podman run --rm -d --name test-container-3 alpine sh -c "trap 'exit 0' TERM; while true; do sleep 1; done"
+    podman run --rm -d --memory=512k --name test-container-1 alpine sh -c "trap 'exit 0' TERM; while true; do sleep 1; done"
+    podman run --rm -d --memory=512k --name test-container-2 alpine sh -c "trap 'exit 0' TERM; while true; do sleep 1; done"
+    podman run --rm -d --memory=512k --name test-container-3 alpine sh -c "trap 'exit 0' TERM; while true; do sleep 1; done"
 
 stop-test-container:
     podman stop test-container-1
