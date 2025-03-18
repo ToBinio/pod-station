420166fae4039c140bfe70d4548acc60abaa2888
tobinio <Tobias.frischmann1@gmail.com>
2025-03-18 14:11:39 +0100

    use WS to update data in frontend

---
+++ frontend/composables/useContainers.ts
@@ -0,0 +1,27 @@
+import type { Container } from "~/utils/containers";
+
+export async function useContainers() {
+  const config = useRuntimeConfig();
+
+  const containers = ref<Container[]>([]);
+
+  const { data: fetchContainers } = await useFetch<Container[]>("/containers", {
+    baseURL: config.public.baseURL as string,
+  });
+
+  const { data: wsContainers } = useWebSocket<string>("ws://localhost:8080/ws");
+
+  watch(
+    fetchContainers,
+    (data) => {
+      if (data) containers.value = data;
+    },
+    { immediate: true },
+  );
+
+  watch(wsContainers, (data) => {
+    if (data) containers.value = JSON.parse(data);
+  });
+
+  return containers;
+}
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-import type { Container } from "~/utils/containers";
-
-const config = useRuntimeConfig();
-
-const { data: containers } = await useFetch<Container[]>("/containers", {
-    baseURL: config.public.baseURL as string,
-});
+const containers = await useContainers();
 </script>
 
 <template>
     <h1 class="text-3xl font-bold text-center p-4">Pod Station</h1>
-    <div v-if="containers !== null">
+    <div>
         <ContainerGroup :containers="containers" />
     </div>
 </template>
